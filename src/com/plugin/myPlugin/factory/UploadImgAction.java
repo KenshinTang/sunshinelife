5333dcf62354bc0837640af6057f75dc6b0b78f4
princebb <RLwx8eH4VVnuJn>
2017-08-28 17:34:27 +0800

    因为阿里云里已经有okhttp，所以去掉了xUtilsJar包

--- src/com/plugin/myPlugin/factory/UploadImgAction.java
+++ src/com/plugin/myPlugin/factory/UploadImgAction.java
@@ -7,21 +7,15 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
-import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
-import android.webkit.MimeTypeMap;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
@@ -37,12 +31,6 @@ import com.alibaba.sdk.android.oss.common.auth.OSSStsTokenCredentialProvider;
 import com.alibaba.sdk.android.oss.internal.OSSAsyncTask;
 import com.alibaba.sdk.android.oss.model.PutObjectRequest;
 import com.alibaba.sdk.android.oss.model.PutObjectResult;
-import com.lidroid.xutils.HttpUtils;
-import com.lidroid.xutils.exception.HttpException;
-import com.lidroid.xutils.http.RequestParams;
-import com.lidroid.xutils.http.ResponseInfo;
-import com.lidroid.xutils.http.callback.RequestCallBack;
-import com.lidroid.xutils.http.client.HttpRequest;
 import com.yunlinker.ygsh.R;
 
 import org.apache.cordova.CallbackContext;
@@ -51,22 +39,20 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.UUID;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Created by YX on 2017/8/20.
@@ -87,13 +73,9 @@ public class UploadImgAction extends IPluginAction {
     private String mPath = Environment.getExternalStorageDirectory().getPath()+"/myHead/";
     private String mFilename = "sunshinelife.jpg";
     private Uri imageUri = Uri.parse("file:///"+Environment.getExternalStorageDirectory().getPath()+"/"+mFilename);
-    public static final String SUCCESS = "success";
-    public static final String FAILURE = "failed";
-    private static final int TIME_OUT = 5 * 60 * 1000; //超时时间
-    private static final String CHARSET = "utf-8"; //设置编码
     private JSONObject mJSONObject;
     private CallbackContext mCallbackContext;
-
+    private OSS oss;
     @Override
     public void doAction(final CordovaPlugin plugin, JSONObject jsonObject, CallbackContext callbackContext) {
         LayoutInflater inflater = LayoutInflater.from(plugin.cordova.getActivity());
@@ -151,10 +133,10 @@ public class UploadImgAction extends IPluginAction {
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         switch (requestCode) {
             case TAKE_TYPE:
-                cropPhoto(imageUri,CROP_TYPE);//裁剪图片
+                cropPhoto(imageUri);//裁剪图片
                 break;
             case ALBUM_TYPE:
-                cropPhoto(imageUri,CROP_TYPE);
+                cropPhoto(intent.getData());
                 break;
             case CROP_TYPE:
                 if (imageUri != null) {
@@ -174,32 +156,35 @@ public class UploadImgAction extends IPluginAction {
                 break;
         }
     }
-
-    private OSS oss;
     /**
      * 上传头像
      */
     private void uploadPic(final String filePath) {
-//      final File file = new File(filePath);
         try {
             final String url = mJSONObject.getString("url");
             String timestamp = mJSONObject.getString("timestamp");
             String sign = mJSONObject.getString("sign");
-            final HashMap<String, String> params = new HashMap<>();
-            params.put("timestamp", timestamp);
-            params.put("sign", sign);
-            RequestParams requestParams = new RequestParams();
-            String sendUrl = getUrl(url, params);
-//            String mime = MimeTypeMap.getSingleton().getMimeTypeFromExtension("png");
-//            requestParams.addBodyParameter("imgFile", new File(filePath), mime);
-
-            HttpUtils httpUtils = new HttpUtils();
-            httpUtils.send(HttpRequest.HttpMethod.POST, sendUrl, requestParams, new RequestCallBack<Object>() {
+            OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody formBody = new FormBody.Builder()
+                    .add("timestamp", timestamp)
+                    .add("sign", sign)
+                    .build();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(formBody)
+                    .build();
+            Call call = okHttpClient.newCall(request);
+            call.enqueue(new Callback() {
                 @Override
-                public void onSuccess(ResponseInfo<Object> responseInfo) {
-                    Log.d("kenshin", "success:" + responseInfo.result.toString());
-                    try {
+                public void onFailure(Call call, IOException e) {
+                }
 
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    String str = response.body().string();
+                    Log.i("allen", "uploadPic: " + str);
+                    JSONObject resultJb = null;
+                    try {
 //                        {"status":"200",
 //                                "AccessKeyId":"STS.GEtzwf1SXmUgn3Wkwc3yBtRmD",
 //                                "AccessKeySecret":"CsXxaA2sCVC7LLvLVksfw6dSLWpqJiy8gSVKaWUfncmn",
@@ -207,13 +192,13 @@ public class UploadImgAction extends IPluginAction {
 //                                ,"Expiration":"2017-08-26T10:42:04Z",
 //                                "dir":"2017-08",
 //                                "filename":"1503743224223"}
-                        JSONObject resultJb = new JSONObject(responseInfo.result.toString());
+                        resultJb = new JSONObject(str);
                         String AccessKeyId = resultJb.getString("AccessKeyId");
                         String AccessKeySecret = resultJb.getString("AccessKeySecret");
                         String SecurityToken = resultJb.getString("SecurityToken");
                         String dir = resultJb.getString("dir");
                         String filename = resultJb.getString("filename");
-                        String imgName = dir + "/" + filename + ".jpg";
+                        final String imgName = dir + "/" + filename + ".jpg";
                         OSSCredentialProvider credentialProvider = new OSSStsTokenCredentialProvider(AccessKeyId, AccessKeySecret, SecurityToken);
                         ClientConfiguration conf = new ClientConfiguration();
                         conf.setConnectionTimeout(15 * 1000); // 连接超时，默认15秒
@@ -234,11 +219,25 @@ public class UploadImgAction extends IPluginAction {
                         }
 
                         PutObjectRequest put = new PutObjectRequest("ygsh", imgName, filePath);
-
                         OSSAsyncTask task = oss.asyncPutObject(put, new OSSCompletedCallback<PutObjectRequest, PutObjectResult>() {
                             @Override
                             public void onSuccess(PutObjectRequest request, PutObjectResult result) {
                                 Log.d("kenshin", "UploadSuccess");
+                                JSONObject dataObject = new JSONObject();
+                                JSONObject callbackJsonObject = new JSONObject();
+                                try {
+                                    dataObject.put("data", callbackJsonObject);
+//                    {code: 成功1，失败0, msg: 描述, imgUrl: [2017-03/12345.jpg,2017-03/12346.jpg]}
+                                    JSONArray array = new JSONArray();
+                                    array.put(imgName);
+                                    callbackJsonObject.put("code", 1);
+                                    callbackJsonObject.put("msg", "上传成功");
+                                    callbackJsonObject.put("imgUrl", array);
+                                    mCallbackContext.success(dataObject);
+                                    Log.i("allen", "callbackMessage : " + dataObject.toString());
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
                             }
                             @Override
                             public void onFailure(PutObjectRequest request, ClientException clientExcepion, ServiceException serviceException) {
@@ -256,136 +255,14 @@ public class UploadImgAction extends IPluginAction {
                                 }
                             }
                         });
-
-                        JSONObject dataObject = new JSONObject();
-                        JSONObject callbackJsonObject = new JSONObject();
-                        dataObject.put("data", callbackJsonObject);
-//                    {code: 成功1，失败0, msg: 描述, imgUrl: [2017-03/12345.jpg,2017-03/12346.jpg]}
-                        JSONArray array = new JSONArray();
-                        array.put(imgName);
-                        callbackJsonObject.put("code", 1);
-                        callbackJsonObject.put("msg", "上传成功");
-                        callbackJsonObject.put("imgUrl", array);
-                        mCallbackContext.success(dataObject);
-                        Log.i("allen", "callbackMessage : " + dataObject.toString());
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
-
-                @Override
-                public void onFailure(HttpException e, String s) {
-
-                }
             });
-
-
-//            Executors.newCachedThreadPool().execute(new Runnable() {
-//                @Override
-//                public void run() {
-//                    uploadFile(file, getUrl(url, params));
-//                }
-//            });
         } catch (JSONException e) {
             e.printStackTrace();
         }
-    }
-
-    /**
-     * android上传文件到服务器
-     *
-     * @param file       需要上传的文件
-     * @param requestURL 请求的rul
-     * @return 返回响应的内容
-     */
-    public String uploadFile(File file, String requestURL) {
-        String result = null;
-        String BOUNDARY = UUID.randomUUID().toString(); //边界标识 随机生成
-        String PREFIX = "--", LINE_END = "\r\n";
-        String CONTENT_TYPE = "multipart/form-data"; //内容类型
-        if (TextUtils.isEmpty(requestURL)) {
-            Log.i("allen", "请求url为空");
-            return FAILURE;
-        }
-        try {
-            Log.i("allen", "传入的上传url" + requestURL);
-            URL url = new URL(requestURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(TIME_OUT);
-            conn.setConnectTimeout(TIME_OUT);
-            conn.setDoInput(true); //允许输入流
-            conn.setDoOutput(true); //允许输出流
-            conn.setUseCaches(false); //不允许使用缓存
-            conn.setRequestMethod("POST"); //请求方式
-            conn.setRequestProperty("Charset", CHARSET);
-            //设置编码
-            conn.setRequestProperty("connection", "keep-alive");
-            conn.setRequestProperty("Content-Type", CONTENT_TYPE + ";boundary=" + BOUNDARY);
-            if (file != null) {
-                /** * 当文件不为空，把文件包装并且上传 */
-                OutputStream outputSteam = conn.getOutputStream();
-                DataOutputStream dos = new DataOutputStream(outputSteam);
-                /**
-                 * 这里重点注意：
-                 * name里面的值为服务器端需要key 只有这个key 才可以得到对应的文件
-                 * filename是文件的名字，包含后缀名的 比如:abc.png
-                 */
-                dos.write((PREFIX + BOUNDARY + LINE_END + "Content-Disposition: form-data; mFilename=\"img\"; filename=\"" + file.getName() + "\"" + LINE_END + "Content-Type: application/octet-stream; charset=" + CHARSET + LINE_END + LINE_END).getBytes());
-                InputStream is = new FileInputStream(file);
-                byte[] bytes = new byte[1024];
-                int len = 0;
-                while ((len = is.read(bytes)) != -1) {
-                    dos.write(bytes, 0, len);
-                }
-                is.close();
-                dos.write(LINE_END.getBytes());
-                byte[] end_data = (PREFIX + BOUNDARY + PREFIX + LINE_END).getBytes();
-                dos.write(end_data);
-                dos.flush();
-                /**
-                 * 获取响应码 200=成功
-                 * 当响应成功，获取响应的流
-                 */
-                int res = conn.getResponseCode();
-                Log.i("allen", "response code:" + res);
-                if (res == 200) {
-                    InputStream input = conn.getInputStream();
-                    StringBuffer sb1 = new StringBuffer();
-                    int ss;
-                    while ((ss = input.read()) != -1) {
-                        sb1.append((char) ss);
-                    }
-                    result = sb1.toString();
-                    Log.i("allen", "result : " + result);
-                    JSONObject jsonObject = new JSONObject(result);
-                    String dir = jsonObject.getString("dir");
-                    String filename = jsonObject.getString("filename");
-                    String imgName = dir + "/" + filename + ".jpg";
-                    JSONObject callbackJsonObject = new JSONObject();
-//                    {code: 成功1，失败0, msg: 描述, imgUrl: [2017-03/12345.jpg,2017-03/12346.jpg]}
-                    JSONArray array = new JSONArray();
-                    array.put(imgName);
-                    callbackJsonObject.put("code", 1);
-                    callbackJsonObject.put("msg", "上传成功");
-                    callbackJsonObject.put("imgUrl", array);
-                    mCallbackContext.success(callbackJsonObject);
-                    Log.i("allen", "callbackMessage : " + callbackJsonObject.toString());
-                    return SUCCESS;
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        JSONObject errorObject = null;
-        try {
-            errorObject = new JSONObject();
-            errorObject.put("msg", "上传失败");
-            errorObject.put("code", 0);
-            mCallbackContext.error(errorObject);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return FAILURE;
     }
 
     @Override
@@ -431,20 +308,26 @@ public class UploadImgAction extends IPluginAction {
         albumIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
         mPlugin.cordova.startActivityForResult(mPlugin, albumIntent, ALBUM_TYPE);
     }
-    private void cropPhoto(Uri uri,int requestCode){
+
+    /**
+     * 裁剪图片
+     *
+     * @param data
+     */
+    private void cropPhoto(Uri data) {
         Intent intent = new Intent("com.android.camera.action.CROP");
-        intent.setDataAndType(uri, "image/*");
+        intent.setDataAndType(data, "image/*");
         intent.putExtra("crop", "true");
         intent.putExtra("aspectX", 1);
         intent.putExtra("aspectY", 1);
         intent.putExtra("outputX", 800);
         intent.putExtra("outputY", 800);
         intent.putExtra("scale", true);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
         intent.putExtra("return-data", false);
         intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
         intent.putExtra("noFaceDetection", true); // no face detection
-        mPlugin.cordova.startActivityForResult(mPlugin, intent, requestCode);
+        mPlugin.cordova.startActivityForResult(mPlugin, intent, CROP_TYPE);
     }
 
     /**
@@ -473,37 +356,6 @@ public class UploadImgAction extends IPluginAction {
                 e.printStackTrace();
             }
         }
-    }
-
-    /**
-     * 拼接URL
-     *
-     * @param baseUrl 接口请求地址
-     * @param params  请求参数
-     * @return 拼接的url
-     */
-    private static String getUrl(String baseUrl, HashMap<String, String> params) {
-        String url = baseUrl;
-        // 添加url参数
-        if (params != null) {
-            Iterator<String> it = params.keySet().iterator();
-            StringBuffer sb = null;
-            while (it.hasNext()) {
-                String key = it.next();
-                String value = params.get(key);
-                if (sb == null) {
-                    sb = new StringBuffer();
-                    sb.append("?");
-                } else {
-                    sb.append("&");
-                }
-                sb.append(key);
-                sb.append("=");
-                sb.append(value);
-            }
-            url += sb.toString();
-        }
-        return url;
     }
 
     private Bitmap decodeUriAsBitmap(Uri uri) {
