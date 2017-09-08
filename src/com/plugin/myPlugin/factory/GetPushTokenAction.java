189d69d6b3688f633e50798085b747865ee3a159
KenshinTang <28135055@qq.com>
2017-09-08 21:40:13 +0800

    按照新的接口文档更新获取推送token的回调.

--- src/com/plugin/myPlugin/factory/GetPushTokenAction.java
+++ src/com/plugin/myPlugin/factory/GetPushTokenAction.java
@@ -15,7 +15,7 @@ import org.json.JSONObject;
  */
 
 public class GetPushTokenAction extends IPluginAction {
-    //{"code":1成功,0失败，"msg":"描述”,”type","友盟、极光 推送名字，如果未获取或者没有推送端则为空","token":"设置token"}
+    //{data:{"code":1成功,0失败，"msg":"描述”,”type","友盟、极光 推送名字，如果未获取或者没有推送端则为空", devicetype:(1安卓, 2苹果), devicetoken: “”}}
     @Override
     public void doAction(CordovaPlugin plugin, JSONObject jsonObject, CallbackContext callbackContext) {
         String token = PushAgent.getInstance(plugin.cordova.getActivity()).getRegistrationId();
@@ -26,7 +26,8 @@ public class GetPushTokenAction extends IPluginAction {
             jo.put("code", isValid ? 1 : 0);
             jo.put("msg", isValid ? "获取推送Token成功" : "获取推送Token失败");
             jo.put("type", "友盟");
-            jo.put("token", token);
+            jo.put("devicetype", 1);
+            jo.put("devicetoken", token);
             JSONObject result = JsonWrapUtils.wrapData(jo);
             if (isValid) {
                 callbackContext.success(result);
